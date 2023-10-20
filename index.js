cfc9098d43d86fe2e1054984b3291208fa474a89
Aayush Regmi <aayureg10@gmail.com>
2023-10-20 12:29:14 +0545

    Add singleRound function

--- index.js
+++ index.js
@@ -14,3 +14,31 @@ function getCompChoice() {
   }
 }
 
+function singleRound(playerSel, compSel) {
+  playerSel = playerSel.toLowerCase();
+  if (playerSel===choices[0]&&compSel===choices[1]){
+    return 'Paper beats rock! You lose'
+  }else if(playerSel===choices[1]&& compSel==choices[2]){
+    return 'Scissors beats paper! You lose'
+  }
+  else if(playerSel===choices[2]&& compSel==choices[0]){
+    return 'Rock beats Scissors! You lose'
+  }
+  else if(playerSel===choices[1]&& compSel==choices[0]){
+    return 'Paper beats rock! You win'
+  }
+  else if(playerSel===choices[2]&& compSel==choices[1]){
+    return 'Scissors beats Paper! You win'
+  }
+  else if(playerSel===choices[0]&& compSel==choices[2]){
+    return 'Rock beats Scissors! You win'
+  }else{
+    return 'It is a draw'
+  }
+  
+}
+
+let playerSelection='rock'
+let compSelection=getCompChoice()
+
+console.log(singleRound(playerSelection,compSelection))
